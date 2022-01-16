a9fdb013a19b369a7e52fb9afa21aca62a8e31c8
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-01-16 22:16:05 +0530

    Initial commit

---
+++ src/main/java/InvalidUserException.java
@@ -0,0 +1,18 @@
+
+public class InvalidUserException extends Exception {
+    public InvalidUserException(String message) {
+        super(message);
+    }
+
+    public enum ExpectedException {
+        ;
+
+        public static ExpectedException none() {
+            return null;
+        }
+
+        public void expect(Class<InvalidUserException> invalidUserExceptionClass) {
+        }
+    }
+
+}
